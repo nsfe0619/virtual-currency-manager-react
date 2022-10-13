0ba395472917c3808e74af14f9ae378a8e022906
DESKTOP-K5JB2G9\nsfe <nsfe0619@gmail.com>
2022-10-13 23:28:00 +0800

    push

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter, Route, Routes, Link } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Home from "./page/Home";
 import FirstPage from "./page/FirstPage";
 import SecondPage from "./page/SecondPage";
@@ -9,104 +9,83 @@ import FivePage from "./page/FivePage";
 import SixPage from "./page/SixPage";
 import SevenPage from "./page/SevenPage";
 import EightPage from "./page/EightPage";
+import NightPage from "./page/NightPage";
+import TenPage from "./page/TenPage";
+import DataPage from "./page/DataPage";
 import { Provider } from "react-redux";
 import store from "./store/store";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { LinkContainer } from 'react-router-bootstrap'
+
 
 const App = () => {
   return (
     <Provider store={store}>
-    <div>
-      <HashRouter>
-        <nav className="px-5 flex items-center h-[60px] bg-indigo-500 text-white">
-          <h1 className="mr-auto text-2xl">React TodoList</h1>
-          <ul className="flex">
-            <li className="mr-3">
-              <Link
-                to="/"
-                className="border p-3 hover:bg-indigo-600 duration-500"
-              >
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/FirstPage"
-                className="border p-3 hover:bg-indigo-600 duration-500"
-              >
-                FirstPage
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/SecondPage"
-                className="border p-3 hover:bg-indigo-600 duration-500"
-              >
-                SecondPage
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/ThreePage"
-                className="border p-3 hover:bg-indigo-600 duration-500"
-              >
-                ThreePage
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/FourPage"
-                className="border p-3 hover:bg-indigo-600 duration-500"
-              >
-                FourPage
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/FivePage"
-                className="border p-3 hover:bg-indigo-600 duration-500"
-              >
-                FivePage
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/SixPage"
-                className="border p-3 hover:bg-indigo-600 duration-500"
-              >
-                SixPage
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/SevenPage"
-                className="border p-3 hover:bg-indigo-600 duration-500"
-              >
-                SevenPage
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/EightPage"
-                className="border p-3 hover:bg-indigo-600 duration-500"
-              >
-                EightPage
-              </Link>
-            </li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/FirstPage" element={<FirstPage />} />
-          <Route path="/SecondPage" element={<SecondPage />} />
-          <Route path="/ThreePage" element={<ThreePage />} />
-          <Route path="/FourPage" element={<FourPage />} />
-          <Route path="/FivePage" element={<FivePage />} />
-          <Route path="/SixPage" element={<SixPage />} />
-          <Route path="/SevenPage" element={<SevenPage />} />
-          <Route path="/EightPage" element={<EightPage />} />
-        </Routes>
-      </HashRouter>
-    </div>
+      <div>
+        <HashRouter>
+          <Navbar bg="light" expand="lg">
+            <Container>
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                  <LinkContainer to="/DataPage">
+                    <Nav.Link>DataPage</Nav.Link>
+                  </LinkContainer>
+                  <NavDropdown title="TestPage" id="basic-nav-dropdown">
+                    <LinkContainer to="/FirstPage">
+                      <Nav.Link>FirstPage</Nav.Link>
+                    </LinkContainer>
+                    <LinkContainer to="/SecondPage">
+                      <Nav.Link>SecondPage</Nav.Link>
+                    </LinkContainer>
+                    <LinkContainer to="/ThreePage">
+                      <Nav.Link>ThreePage</Nav.Link>
+                    </LinkContainer>
+                    <LinkContainer to="/FourPage">
+                      <Nav.Link>FourPage</Nav.Link>
+                    </LinkContainer>
+                    <LinkContainer to="/FivePage">
+                      <Nav.Link>FivePage</Nav.Link>
+                    </LinkContainer>
+                    <LinkContainer to="/SixPage">
+                      <Nav.Link>SixPage</Nav.Link>
+                    </LinkContainer>
+                    <LinkContainer to="/SevenPage">
+                      <Nav.Link>SevenPage</Nav.Link>
+                    </LinkContainer>
+                    <LinkContainer to="/EightPage">
+                      <Nav.Link>EightPage</Nav.Link>
+                    </LinkContainer>
+                    <LinkContainer to="/NightPage">
+                      <Nav.Link>NightPage</Nav.Link>
+                    </LinkContainer>
+                    <LinkContainer to="/TenPage">
+                      <Nav.Link>TenPage</Nav.Link>
+                    </LinkContainer>
+                  </NavDropdown>
+                </Nav>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/FirstPage" element={<FirstPage />} />
+            <Route path="/SecondPage" element={<SecondPage />} />
+            <Route path="/ThreePage" element={<ThreePage />} />
+            <Route path="/FourPage" element={<FourPage />} />
+            <Route path="/FivePage" element={<FivePage />} />
+            <Route path="/SixPage" element={<SixPage />} />
+            <Route path="/SevenPage" element={<SevenPage />} />
+            <Route path="/EightPage" element={<EightPage />} />
+            <Route path="/NightPage" element={<NightPage />} />
+            <Route path="/TenPage" element={<TenPage />} />
+            <Route path="/DataPage" element={<DataPage />} />
+          </Routes>
+        </HashRouter>
+      </div>
     </Provider>
   );
 };
